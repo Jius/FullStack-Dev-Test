a752ff39fcf9b23066011cfdb59c26111bdc1eac
Jérémy Berthelier <jeremy.berthelier@unkle.fr>
2022-09-06 10:03:02 +0200

    Update test for controllers

--- server/test/controllers/interventions_controller_test.rb
+++ server/test/controllers/interventions_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class InterventionsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @intervention = interventions(:one)
+    @intervention = interventions(:gotham_bruce)
   end
 
   test "should get index" do
@@ -12,10 +12,15 @@ class InterventionsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create intervention" do
     assert_difference('Intervention.count') do
-      post interventions_url, params: { intervention: { address: @intervention.address, city: @intervention.city, company_id: @intervention.company_id, customer_id: @intervention.customer_id, ref_panel: @intervention.ref_panel, started_at: @intervention.started_at, total_panels: @intervention.total_panels, type_panel: @intervention.type_panel, zipcode: @intervention.zipcode } }, as: :json
+      post interventions_url, params: { intervention: { address: @intervention.address, city: @intervention.city, company_attributes: {name: @intervention.company.name, siren: @intervention.company.siren}, customer_attributes: {name: @intervention.customer.name, email: @intervention.customer.email, phone: @intervention.customer.phone}, ref_panel: @intervention.ref_panel, started_at: @intervention.started_at, total_panels: @intervention.total_panels, type_panel: @intervention.type_panel, zipcode: @intervention.zipcode } }, as: :json
     end
 
     assert_response 201
+  end
+
+  test "shouldn't create intervention with incorrect params" do
+    post interventions_url, params: { intervention: { bad_params: 'Hello world' } }, as: :json
+    assert_response 422
   end
 
   test "should show intervention" do
@@ -32,7 +37,6 @@ class InterventionsControllerTest < ActionDispatch::IntegrationTest
     assert_difference('Intervention.count', -1) do
       delete intervention_url(@intervention), as: :json
     end
-
     assert_response 204
   end
 end
