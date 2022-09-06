9f209bcd134afc307bc0fdb42cbd415b24b9e4c4
Jérémy Berthelier <jeremy.berthelier@unkle.fr>
2022-09-06 10:12:46 +0200

    Refactoring

--- server/app/models/customer.rb
+++ server/app/models/customer.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class Customer < ApplicationRecord
   has_many :interventions
   validates :name, :email, :phone, presence: true
-  validates :email,email: {mode: :strict, require_fqdn: true}
+  validates :email, email: {mode: :strict, require_fqdn: true}
 end
--- server/app/models/intervention.rb
+++ server/app/models/intervention.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 class Intervention < ApplicationRecord
   belongs_to :customer
   belongs_to :company
   enum type_panel: [:photovoltaic, :hybrid]
+
   validates :started_at, :total_panels, :ref_panel, :address, :zipcode, :city, presence: true
-  validates :ref_panel, :total_panels, numericality: true
+  validates :total_panels, :ref_panel, numericality: true
   validates :ref_panel, length: { is: 6 }
 
-  before_create :set_type_panel_default
-
+  before_validation :set_type_panel_default
   accepts_nested_attributes_for :customer, :company
 
   private
